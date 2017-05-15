64d14d95cf04e1a3241c3625d71c0a2ed426fca6
Giacomo Bergami <giacomo@openmailbox.org>
2017-05-15 23:54:10 +0200

    Possible final version for doing statistics (2)

--- src/main/java/it/giacomobergami/readingFlinkJson/utils/queries/Query.java
+++ src/main/java/it/giacomobergami/readingFlinkJson/utils/queries/Query.java
@@ -1,3 +1,21 @@
+/*
+ * This file is part of readingFlinkJson
+ *
+ * Copyright (C) 2017 - Giacomo Bergami
+ *
+ * readingFlinkJson is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * readingFlinkJson is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with readingFlinkJson. If not, see <http://www.gnu.org/licenses/>.
+ */
 package it.giacomobergami.readingFlinkJson.utils.queries;
 
 import it.giacomobergami.readingFlinkJson.node.IUniformView;
@@ -5,7 +23,7 @@ import it.giacomobergami.readingFlinkJson.node.IUniformView;
 import java.util.function.Function;
 
 /**
- * Defining what a query is
+ * Defining what a query is, that is transforming a IUniformView to some result
  */
 public interface Query<K> extends Function<IUniformView, K> {
 }
